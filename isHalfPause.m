994df7a7d0976d58098fe21b9db3f2fccbd6842d
test <j.b.baczewska@gmail.com>
2021-01-02 12:13:12 +0100

    Dodanie funkcji sprawdzającej czy pauza jest pauzą półnutową

---
+++ isHalfPause.m
@@ -0,0 +1,13 @@
+function  flag = isHalfPause(posLines3, posLines4, note, symBBox)
+%ISHALFPAUSE Checks if pause is whole or half
+           
+            diffUp=abs(symBBox(2)-posLines4);
+            diffDown=abs(symBBox(2)+symBBox(4)-posLines3);
+            
+            if  diffDown< diffUp
+                 flag = true;
+            else
+                flag = false;
+            end
+end
+
