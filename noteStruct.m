0d7f5ef6ab8ac51a7d73f669e21ba61b53935a37
test <j.b.baczewska@gmail.com>
2021-01-15 22:05:29 +0100

    Dodanie wysokości na sztywno w kreślonych nutach w tworzeniu bazy i strukturze

--- noteStruct.m
+++ noteStruct.m
@@ -1,9 +1,9 @@
-function Note = noteStruct(id, name, File, rotable)
+function Note = noteStruct(id, name, File, rotable, height)
 %creates note structure from *.jpg: resizes, crops image
     im = double(rgb2gray(imread(File)))/255;
     im = imbinarize(im, 0.9);
     measurement = regionprops(im, 'Image');
     im = imresize(measurement(1).Image, [128 64]);
     
-    Note = struct('Id', id, 'Name', name, 'Image', im, 'Rotable', rotable); 
+    Note = struct('Id', id, 'Name', name, 'Image', im, 'Rotable', rotable, 'Height', height); 
 end
